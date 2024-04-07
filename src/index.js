3faf4bb12961939d793ddec23bde9b4d30639f0b
Kaio Silveira <silveira.kaio@icloud.com>
2024-04-07 19:14:33 +0100

    extract summer conditions to its own function

--- src/index.js
+++ src/index.js
@@ -15,7 +15,11 @@ export class ManagedDate {
 export function calculateCharge(aDate, plan, quantity) {
   let charge;
 
-  if (!aDate.isBefore(plan.summerStart) && !aDate.isAfter(plan.summerEnd)) {
+  function summer() {
+    return !aDate.isBefore(plan.summerStart) && !aDate.isAfter(plan.summerEnd);
+  }
+
+  if (summer()) {
     charge = quantity * plan.summerRate;
   } else {
     charge = quantity * plan.regularRate + plan.regularServiceCharge;
