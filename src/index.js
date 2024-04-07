0992b97cc4d5196d371ffb1cfe66e3771fcaca27
Kaio Silveira <silveira.kaio@icloud.com>
2024-04-07 19:13:26 +0100

    setup working example

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
-export function toBeRefactored() {
-  return 'Hello, world!';
+export class ManagedDate {
+  constructor(date) {
+    this.date = new Date(date);
+  }
+
+  isBefore(otherDate) {
+    return this.date < otherDate;
+  }
+
+  isAfter(otherDate) {
+    return this.date > otherDate;
+  }
+}
+
+export function calculateCharge(aDate, plan, quantity) {
+  let charge;
+
+  if (!aDate.isBefore(plan.summerStart) && !aDate.isAfter(plan.summerEnd)) {
+    charge = quantity * plan.summerRate;
+  } else {
+    charge = quantity * plan.regularRate + plan.regularServiceCharge;
+  }
+
+  return charge;
 }
--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,31 @@
-import { toBeRefactored } from './index';
+import { calculateCharge, ManagedDate } from './index';
 
-describe('functionToBeRefactored', () => {
-  it('should work', () => {
-    expect(toBeRefactored()).toEqual('Hello, world!');
+describe('calculateCharge', () => {
+  it('should calculate the charge for the summer', () => {
+    const quantity = 10;
+    const aDate = new ManagedDate('2021-07-01');
+    const plan = {
+      summerStart: new Date('2021-06-01'),
+      summerEnd: new Date('2021-09-01'),
+      summerRate: 1,
+      regularRate: 2,
+      regularServiceCharge: 3,
+    };
+
+    expect(calculateCharge(aDate, plan, quantity)).toEqual(10);
+  });
+
+  it('should calculate the charge for the other seasons', () => {
+    const quantity = 10;
+    const aDate = new ManagedDate(new Date('2021-01-01'));
+    const plan = {
+      summerStart: new Date('2021-06-01'),
+      summerEnd: new Date('2021-09-01'),
+      summerRate: 1,
+      regularRate: 2,
+      regularServiceCharge: 3,
+    };
+
+    expect(calculateCharge(aDate, plan, quantity)).toEqual(23);
   });
 });
